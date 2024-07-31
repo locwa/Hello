46b4e43ddc3fdbc38b791bd3a7bf6cf2b9a6f54b
locwa <94048557+locwa@users.noreply.github.com.>
2024-07-31 21:10:49 +0800

    added navbar placeholder

--- src/private/includes/homepage.php
+++ src/private/includes/homepage.php
@@ -5,6 +5,9 @@
     include_once("header.php")
 ?>
 <body>
+    <nav class="navbar navbar-expand-lg" id="navBar" style="background-color: gray;">
+        <div class="container-fluid"> hello</div>
+    </nav>
     
 <?php include_once("js_components.php")?>
 </body>
