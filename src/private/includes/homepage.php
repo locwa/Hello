f318a2a9c2f08e6842ec7a468d298f111645fb8c
locwa <94048557+locwa@users.noreply.github.com.>
2024-08-01 16:03:17 +0800

    added hero

--- src/private/includes/homepage.php
+++ src/private/includes/homepage.php
@@ -4,11 +4,15 @@
     $page_name = "Hello | A messaging app";
     include_once("header.php")
 ?>
-<body>
-    <nav class="navbar navbar-expand-lg" id="navBar" style="background-color: gray;">
+<body class="container-fluid vh-100 m-0 g-0">
+    <nav class="navbar navbar-expand-lg" id="navBar">
         <div class="container-fluid"> hello</div>
     </nav>
-    
+    <div class="container-fluid hero">
+        <div class="container-sm g-0" style="margin-left: 0.5rem">
+            <h1 class="headline">Connect with friends. Whenever, Wherever.</h1>
+        </div>
+    </div>
 <?php include_once("js_components.php")?>
 </body>
 </html>
