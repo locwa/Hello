279fd22e8bc558219e61f854462bdfccfe29ce84
locwa <94048557+locwa@users.noreply.github.com.>
2024-09-14 20:36:54 +0800

    logo refinement

--- src/private/includes/homepage.php
+++ src/private/includes/homepage.php
@@ -7,38 +7,51 @@
 <body class="container-fluid vh-100 m-0 g-0">
     <nav class="navbar navbar-expand-lg" id="navBar">
         <svg
-            width="110"
-            height="55"
-            viewBox="0 0 155.46667 60.177087"
-            version="1.1"
-            id="svg1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:svg="http://www.w3.org/2000/svg">
-            <defs
-                id="defs1">
-                <rect
-                x="35.695824"
-                y="104.04952"
-                width="431.38782"
-                height="219.49133"
-                id="rect1" />
-            </defs>
-            <g
-                id="layer1">
-                <text
-                xml:space="preserve"
-                transform="matrix(0.68621181,0,0,0.71161713,-105.34506,-108.83407)"
-                id="text1"
-                style="font-size:162.667px;white-space:pre;shape-inside:url(#rect1);display:inline;fill:#000000;stroke-width:0"
-                x="12.983988"
-                y="0"><tspan
-                    x="270"
-                    y="256.14248"
-                    id="tspan4"><tspan
-                    style="font-family:Sansilk;-inkscape-font-specification:Sansilk;text-align:center;text-anchor:middle"
-                    id="tspan1">hello</tspan></tspan></text>
-            </g>
+        width="150"
+        height="70"
+        viewBox="0 0 180 46.566696"
+        version="1.1"
+        id="svg1"
+        xml:space="preserve"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:svg="http://www.w3.org/2000/svg"><defs
+            id="defs1"><rect
+            x="35.695824"
+            y="104.04952"
+            width="431.38782"
+            height="219.49133"
+            id="rect1" /><rect
+            x="35.695824"
+            y="104.04952"
+            width="431.38782"
+            height="219.49133"
+            id="rect1-1" /></defs><g
+            id="layer1"
+            transform="translate(90,-6.6905746)"><text
+            xml:space="preserve"
+            transform="matrix(0.75368191,0,0,0.78158514,-112.92602,-82.823726)"
+            id="text1"
+            style="font-size:72.6962px;white-space:pre;shape-inside:url(#rect1);display:inline;fill:#ffffff;stroke-width:0"
+            x="12.983988"
+            y="0"><tspan
+                x="157.93872"
+                y="170.19329"
+                id="tspan3"><tspan
+                style="font-weight:900;font-family:'Noto Sans';-inkscape-font-specification:'Noto Sans Heavy';text-align:center;text-anchor:middle"
+                id="tspan1">hello</tspan></tspan></text><text
+            xml:space="preserve"
+            transform="matrix(0.75368191,0,0,0.78158514,-115.60872,-80.440226)"
+            id="text1-0"
+            style="font-size:72.6962px;white-space:pre;shape-inside:url(#rect1-1);display:inline;fill:#000000;stroke-width:0"
+            x="12.983988"
+            y="0"><tspan
+                x="157.93872"
+                y="170.19329"
+                id="tspan5"><tspan
+                style="font-weight:900;font-family:'Noto Sans';-inkscape-font-specification:'Noto Sans Heavy';text-align:center;text-anchor:middle"
+                id="tspan4">hello</tspan></tspan></text></g>
         </svg>
+
     </nav>
     <div class="container-fluid hero">
         <div class="row">
