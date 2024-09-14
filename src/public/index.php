b60477e64475adf5b17210be4e8cba9739b311a0
locwa <94048557+locwa@users.noreply.github.com.>
2024-09-14 15:38:39 +0800

    restructured files

--- src/public/index.php
+++ src/public/index.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (isset($_SESSION['check']) && $_SESSION['check'] == true){
-
+    include_once("../public/inbox.php");
 }
 else{
     include_once("../private/includes/homepage.php");
