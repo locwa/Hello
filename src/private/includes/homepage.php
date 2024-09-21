a4074bf37d6bba34a9bb21c7b10543fb4f5044b9
locwa <94048557+locwa@users.noreply.github.com.>
2024-09-21 18:24:21 +0800

    restructured svg

--- src/private/includes/homepage.php
+++ src/private/includes/homepage.php
@@ -15,58 +15,42 @@
             xml:space="preserve"
             xmlns="http://www.w3.org/2000/svg"
             xmlns:svg="http://www.w3.org/2000/svg">
-            <defs id="defs1">
-                <rect
+            <defs
+                id="defs1"><rect
                 x="35.695824"
                 y="104.04952"
                 width="431.38782"
                 height="219.49133"
-                id="rect1"/>
-                <rect
-                    x="35.695824"
-                    y="104.04952"
-                    width="431.38782"
-                    height="219.49133"
-                    id="rect1-1"/>
-            </defs>
-            <g
+                id="rect1" /><rect
+                x="35.695824"
+                y="104.04952"
+                width="431.38782"
+                height="219.49133"
+                id="rect1-1" /></defs><g
                 id="layer1"
-                transform="translate(60,-6.6905746)">
-                <text
-                    xml:space="preserve"
-                    transform="matrix(0.75368191,0,0,0.78158514,-112.92602,-82.823726)"
-                    id="text1"
-                    style="font-size:72.6962px;white-space:pre;shape-inside:url(#rect1);display:inline;fill:#ffffff;stroke-width:0"
-                    x="12.983988"
-                    y="0">
-                    <tspan
-                        x="157.93872"
-                        y="170.19329"
-                        id="tspan3">
-                        <tspan
-                        style="font-weight:900;font-family:'Noto Sans';-inkscape-font-specification:'Noto Sans Heavy';text-align:center;text-anchor:middle"
-                        id="tspan1">hello
-                        </tspan>
-                    </tspan>
-                </text>
-                <text
-                    xml:space="preserve"
-                    transform="matrix(0.75368191,0,0,0.78158514,-115.60872,-80.440226)"
-                    id="text1-0"
-                    style="font-size:72.6962px;white-space:pre;shape-inside:url(#rect1-1);display:inline;fill:#000000;stroke-width:0"
-                    x="12.983988"
-                    y="0">
-                    <tspan
-                        x="157.93872"
-                        y="170.19329"
-                        id="tspan5">
-                        <tspan
-                            style="font-weight:900;font-family:'Noto Sans';-inkscape-font-specification:'Noto Sans Heavy';text-align:center;text-anchor:middle"
-                            id="tspan4">hello
-                        </tspan>
-                    </tspan>
-                </text>
-            </g>
+                transform="translate(60,-6.6905746)"><text
+                xml:space="preserve"
+                transform="matrix(0.75368191,0,0,0.78158514,-112.92602,-82.823726)"
+                id="text1"
+                style="font-size:72.6962px;white-space:pre;shape-inside:url(#rect1);display:inline;fill:#ffffff;stroke-width:0"
+                x="12.983988"
+                y="0"><tspan
+                    x="157.93872"
+                    y="170.19329"
+                    id="tspan3"><tspan
+                    style="font-weight:900;font-family:'Noto Sans';-inkscape-font-specification:'Noto Sans Heavy';text-align:center;text-anchor:middle"
+                    id="tspan1">hello</tspan></tspan></text><text
+                xml:space="preserve"
+                transform="matrix(0.75368191,0,0,0.78158514,-115.60872,-80.440226)"
+                id="text1-0"
+                style="font-size:72.6962px;white-space:pre;shape-inside:url(#rect1-1);display:inline;fill:#000000;stroke-width:0"
+                x="12.983988"
+                y="0"><tspan
+                    x="157.93872"
+                    y="170.19329"
+                    id="tspan5"><tspan
+                    style="font-weight:900;font-family:'Noto Sans';-inkscape-font-specification:'Noto Sans Heavy';text-align:center;text-anchor:middle"
+                    id="tspan4">hello</tspan></tspan></text></g>
         </svg>
 
     </nav>
