190ffbf0257f9db70c901bb1b1f021a25cdb0454
locwa <94048557+locwa@users.noreply.github.com.>
2024-09-21 18:23:23 +0800

    restructured svg

--- src/private/includes/homepage.php
+++ src/private/includes/homepage.php
@@ -7,14 +7,28 @@
 <body>
     <nav>
         <svg
-        width="150"
-        height="70"
-        viewBox="0 0 180 46.566696"
-        version="1.1"
-        id="svg1"
-        xml:space="preserve"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:svg="http://www.w3.org/2000/svg">
+            width="150"
+            height="70"
+            viewBox="0 0 180 46.566696"
+            version="1.1"
+            id="svg1"
+            xml:space="preserve"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:svg="http://www.w3.org/2000/svg">
+            <defs id="defs1">
+                <rect
+                x="35.695824"
+                y="104.04952"
+                width="431.38782"
+                height="219.49133"
+                id="rect1"/>
+                <rect
+                    x="35.695824"
+                    y="104.04952"
+                    width="431.38782"
+                    height="219.49133"
+                    id="rect1-1"/>
+            </defs>
             <g
                 id="layer1"
                 transform="translate(60,-6.6905746)">
@@ -25,15 +39,15 @@
                     style="font-size:72.6962px;white-space:pre;shape-inside:url(#rect1);display:inline;fill:#ffffff;stroke-width:0"
                     x="12.983988"
                     y="0">
-                <tspan
-                    x="157.93872"
-                    y="170.19329"
-                    id="tspan3">
                     <tspan
+                        x="157.93872"
+                        y="170.19329"
+                        id="tspan3">
+                        <tspan
                         style="font-weight:900;font-family:'Noto Sans';-inkscape-font-specification:'Noto Sans Heavy';text-align:center;text-anchor:middle"
                         id="tspan1">hello
+                        </tspan>
                     </tspan>
-                </tspan>
                 </text>
                 <text
                     xml:space="preserve"
