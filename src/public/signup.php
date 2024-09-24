c936e0b2c414ca51175cb01fe985dc8702826b50
locwa <94048557+locwa@users.noreply.github.com.>
2024-09-24 22:26:26 +0800

    laid the initial inputs for signup

--- src/public/signup.php
+++ src/public/signup.php
@@ -12,6 +12,48 @@
 ?>
 </head>
 <body>
-    
+    <div class="registration-container">
+        <form action="../private/registration.php" method="post">
+            <input type="text" name="fname">
+            <input type="text" name="lname">
+            <input type="email" name="email">
+            <input type="password" name="password">
+            <div class="birthdate">
+                <select name="month">
+                    <option value="january">January</option>
+                    <option value="feburary">Feburary</option>
+                    <option value="march">March</option>
+                    <option value="april">April</option>
+                    <option value="may">May</option>
+                    <option value="june">June</option>
+                    <option value="july">July</option>
+                    <option value="august">August</option>
+                    <option value="september">September</option>
+                    <option value="october">October</option>
+                    <option value="november">November</option>
+                    <option value="december">December</option>
+                </select>
+                <select name="day">
+                    <?php 
+                        for ($i = 1; $i <= 31; $i++){
+                            echo '<option value="' . $i . '">' . $i . '</option>';      
+                        }
+                    ?>
+                </select>
+                <select name="year">
+                    <?php
+                        for ($i = intval(date("Y")); $i >= 1900; $i--){
+                            echo '<option value="' . $i . '">' . $i . '</option>'; 
+                        }
+                    ?>
+                </select>
+                <select name="gender">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                </select>
+            </div>
+        </form>
+    </div>    
 </body>
 </html>
