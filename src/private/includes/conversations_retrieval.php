fd4f10d9b05a8477332487bbf580276c2526a4cc
locwa <94048557+locwa@users.noreply.github.com.>
2024-09-28 08:37:18 +0800

    added sql query

--- src/private/includes/conversations_retrieval.php
+++ src/private/includes/conversations_retrieval.php
@@ -1,9 +1,19 @@
 <?php
     include_once("dbconnect.php");
-    $id = $_SERVER["id"];
+    $id = $_SESSION["id"];
 
-    $query = "SELECT * FROM conversations WHERE user1 = ? OR user2 = ?";
+    $query =   "SELECT 
+                    c.conversation_id, c.user1, a1.first_name, a1.last_name, c.user2, a2.first_name, a2.last_name
+                FROM 
+                    conversations c
+                JOIN	
+                    accounts a1 ON c.user1 = a1.id
+                JOIN	
+                    accounts a2 ON c.user2 = a2.id
+                WHERE
+                    c.user1 = ? OR c.user2 = ?";
+
     $stmt = $pdo->prepare($query);
-    $stmt->execute($id, $id);
-    
+    $stmt->execute([$id, $id]);
+    $conversations = $stmt->fetch();
 ?>
