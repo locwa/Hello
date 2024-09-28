b129a3514d1b8f3b35ea249bb3ac7557d22ee660
locwa <94048557+locwa@users.noreply.github.com.>
2024-09-28 21:01:24 +0800

    rewrote the code for oop

--- src/private/includes/dbconnect.php
+++ src/private/includes/dbconnect.php
@@ -1,16 +1,22 @@
 <?php
-    $user = "root";
-    $pwd = "";
-    $host = "localhost";
-    $dbname = "hello";
+    class DBConnection extends PDO{
+        public function __construct()
+        {
+            $user = "root";
+            $pwd = "";
+            $host = "localhost";
+            $dbname = "hello";
 
-    $dsn = "mysql:host=". $host .";dbname=". $dbname;
+            $dsn = "mysql:host=". $host .";dbname=". $dbname;
 
-    // connectton to database
-    try{
-        $pdo = new PDO($dsn, $user, $pwd);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // connectton to database
+            try{
+                parent::__construct($dsn, $user, $pwd);
+                $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch (PDOException $e){
+                echo "connection failed:". $e->getMessage();
+            }
+        }
     }
-    catch (PDOException $e){
-        echo "connection failed:". $e->getMessage();
-    }
+    
