e1981b740bef25bedb8f39182e4b73b1fe27efb4
locwa <94048557+locwa@users.noreply.github.com.>
2024-09-28 14:39:21 +0800

    made the code get the conversations and echo them

--- src/private/includes/conversations_retrieval.php
+++ src/private/includes/conversations_retrieval.php
@@ -1,19 +1,36 @@
 <?php
     include_once("dbconnect.php");
     $id = $_SESSION["id"];
+    $first_name = $_SESSION["first_name"];
+    $last_name = $_SESSION["last_name"];
 
-    $query =   "SELECT 
-                    c.conversation_id, c.user1, a1.first_name, a1.last_name, c.user2, a2.first_name, a2.last_name
-                FROM 
-                    conversations c
-                JOIN	
-                    accounts a1 ON c.user1 = a1.id
-                JOIN	
-                    accounts a2 ON c.user2 = a2.id
-                WHERE
-                    c.user1 = ? OR c.user2 = ?";
+    $query =   "SELECT DISTINCT
+                    c.conversation_id, a.id, a.first_name, a.last_name
+                FROM
+                    accounts a
+                INNER JOIN
+                    conversations c ON c.user1 = a.id OR c.user2 = a.id
+                WHERE 
+                    (
+                        (a.first_name <> ?) 
+                        OR (a.last_name <> ?)
+                    ) 
+                    AND 
+                    (
+                        (c.user1 = ?) 
+                        OR (c.user2 = ?)
+                    )
+                ";
 
     $stmt = $pdo->prepare($query);
-    $stmt->execute([$id, $id]);
-    $conversations = $stmt->fetch();
+    $stmt->execute([$first_name, $last_name, $id, $id]);
+    $conversations = $stmt->fetchAll();
+    for ($i = 0; $i < $stmt->rowCount(); $i++){
+        echo "
+        <div class='conversation'>
+                <h4 class='sm'>" . $conversations[$i]['first_name'] . " " . $conversations[$i]['last_name'] . "</h4>
+                <p class='xs'>message</p>
+        </div>
+            ";
+    }  
 ?>
