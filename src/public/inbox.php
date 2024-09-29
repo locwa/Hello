b3115c41cbb7f16693c8113bfa9659cb8c243c1f
locwa <94048557+locwa@users.noreply.github.com.>
2024-09-29 19:42:02 +0800

    styled the message roll

--- src/public/inbox.php
+++ src/public/inbox.php
@@ -65,12 +65,12 @@
         <div class="message-roll">
             <div class="chat received">
                 <div class="bubble-received">
-                    <p class="sm">hi</p>
+                    <p class="chat-text">hi</p>
                 </div>
             </div>
             <div class="chat sent">
                 <div class="bubble-sent">
-                    <p class="sm">hello</p>
+                    <p class="chat-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi eveniet aperiam doloribus rem nam aut? Perferendis, asperiores placeat eveniet temporibus sequi consectetur. Tenetur similique sapiente repudiandae provident debitis! Nam, vitae!</p>
                 </div>
             </div>
         </div>
