95956f4141b47b582255068d99d35b9955c985aa
locwa <94048557+locwa@users.noreply.github.com.>
2024-09-29 21:58:20 +0800

    made the message input into a form

--- src/public/inbox.php
+++ src/public/inbox.php
@@ -163,30 +163,34 @@
                         d="M14 14l1 -1c.928 -.893 2.072 -.893 3 0l3 3" 
                     />
                 </svg>
-                <input class="message-draft" type="text" name="message" placeholder="Aa">
-                <svg  
-                    xmlns="http://www.w3.org/2000/svg"  
-                    width="24"  
-                    height="24"  
-                    viewBox="0 0 24 24"  
-                    fill="none"  
-                    stroke="currentColor"  
-                    stroke-width="2"  
-                    stroke-linecap="round"  
-                    stroke-linejoin="round"  
-                    class="upload-button send">
-                    <path 
-                        stroke="none" 
-                        d="M0 0h24v24H0z" 
-                        fill="none"
-                    />
-                    <path 
-                        d="M10 14l11 -11" 
-                    />
-                    <path 
-                        d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5" 
-                    />
-                </svg>
+                <form action="inbox.php" class="message-draft" method="post">
+                    <input class="message-text" type="text" name="message" placeholder="Aa">
+                    <button type="submit" name="send-message" class="send-message-button">
+                        <svg  
+                            xmlns="http://www.w3.org/2000/svg"  
+                            width="24"  
+                            height="24"  
+                            viewBox="0 0 24 24"  
+                            fill="none"  
+                            stroke="currentColor"  
+                            stroke-width="2"  
+                            stroke-linecap="round"  
+                            stroke-linejoin="round"  
+                            class="upload-button send">
+                            <path 
+                                stroke="none" 
+                                d="M0 0h24v24H0z" 
+                                fill="none"
+                            />
+                            <path 
+                                d="M10 14l11 -11" 
+                            />
+                            <path 
+                                d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5" 
+                            />
+                        </svg>
+                    </button>
+                </form>       
             </div>
         </div>
     </div>
