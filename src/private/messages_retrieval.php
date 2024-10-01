167146387a14cf876249a8a962fc40c2ced7881d
locwa <94048557+locwa@users.noreply.github.com.>
2024-10-02 07:09:37 +0800

    added messages_retrieval file

---
+++ src/private/messages_retrieval.php
@@ -0,0 +1,14 @@
+<?php
+echo "
+            <div class='chat received'>
+                <div class='bubble-received'>
+                    <p class='chat-text'>hi</p>
+                </div>
+            </div>
+            <div class='chat sent'>
+                <div class='bubble-sent'>
+                    <p class='chat-text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi eveniet aperiam doloribus rem nam aut? Perferendis, asperiores placeat eveniet temporibus sequi consectetur. Tenetur similique sapiente repudiandae provident debitis! Nam, vitae!</p>
+                </div>
+            </div>
+";
+
