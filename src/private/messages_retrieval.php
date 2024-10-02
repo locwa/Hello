79e129ba5942abd756e068bfcdfe63109b38ac72
locwa <94048557+locwa@users.noreply.github.com.>
2024-10-02 11:41:19 +0800

    added code to get and echo the messages

--- src/private/messages_retrieval.php
+++ src/private/messages_retrieval.php
@@ -1,14 +1,32 @@
 <?php
-echo "
-            <div class='chat received'>
-                <div class='bubble-received'>
-                    <p class='chat-text'>hi</p>
+    include_once("../private/includes/hello_api.php");
+    session_start();
+    $conv_id = $_GET['c'];
+
+    $messages = new Messages();
+    $row_count = $messages->getMessages($conv_id)->rowCount();
+    $message_roll = $messages->getMessages($conv_id)->fetchAll();
+
+    for($i = 0; $i<$row_count; $i++){
+        $sender_id = $message_roll[$i]['sender_id'];
+        $text_content = $message_roll[$i]['text_content'];
+        if ($sender_id != $_SESSION['id']) {
+            echo "
+                  <div class='chat received'>
+                      <div class='bubble-received'>
+                          <p class='chat-text'>".$text_content."</p>
+                      </div>
+                  </div>
+            ";
+        }
+        else{
+            echo " 
+                <div class='chat sent'>
+                    <div class='bubble-sent'>
+                        <p class='chat-text'>".$text_content."</p>
+                    </div>
                 </div>
-            </div>
-            <div class='chat sent'>
-                <div class='bubble-sent'>
-                    <p class='chat-text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi eveniet aperiam doloribus rem nam aut? Perferendis, asperiores placeat eveniet temporibus sequi consectetur. Tenetur similique sapiente repudiandae provident debitis! Nam, vitae!</p>
-                </div>
-            </div>
-";
+                ";
+        }
+    }
 
