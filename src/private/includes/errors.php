d5e5b8db1e50157a255f800a89e6bebb781665d1
locwa <94048557+locwa@users.noreply.github.com.>
2024-10-17 07:17:57 +0800

    added PHPDoc to the functions

--- src/private/includes/errors.php
+++ src/private/includes/errors.php
@@ -1,6 +1,11 @@
 <?php
     $error_text = "";
-    function signupError($error){
+    /**
+     * Errors pertaining to the signup page
+     *
+     * @param int $error The error code generated
+     */
+    function signupError(int $error){
         switch($error){
             case 1:
                 $error_text = "Please fill in all the required fields.";
@@ -14,6 +19,12 @@
         }
         echo errorText($error_text, 0);
     }
+
+    /**
+     * Errors pertaining to the login page
+     *
+     * @param int $error The error code generated
+     */
     function loginError($error){
         switch($error){
             case 1:
@@ -27,7 +38,14 @@
         echo errorText($error_text, 1);
     }
 
-    function errorText ($error, $token){
+    /**
+     * Generates the HTML of the error banner
+     *
+     * @param string $error The error text
+     * @param int $token An integer that detects the error type
+     * @return string The HTML code generated
+     */
+    function errorText (string $error, int $token){
         if ($token = 0){
             $error_banner = '<div class="signup-banner error-banner">';
         }
